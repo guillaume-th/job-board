4e45f7c54de2edddff0797a0e59053632f37db9a
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2023-10-11 16:10:02 +0200

    :construction: Resolve little merge issues

--- client/src/components/advertisement/AdvertisementContainer.tsx
+++ client/src/components/advertisement/AdvertisementContainer.tsx
@@ -1,21 +1,18 @@
-import { FC,useState } from "react";
+import { FC } from "react";
 import { useQuery } from "../../hooks/useQuery";
 import { Advertisement } from "../../types/advertisement";
 import AdvertisementContent from "./AdvertisementContent";
-import {useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 
 const AdvertisementContainer: FC = () => {
-    const {id} = useParams();
-    const {data,error} = useQuery<Advertisement>("api/advertisements/"+id)
+  const { id } = useParams();
+  const { data, error } = useQuery<Advertisement>("api/advertisements/" + id);
 
-    if(data){
-        return <AdvertisementContent data={data}/>
-    }
-    else {
-        return <p>{error}</p>;
-    }
-
+  if (data) {
+    return <AdvertisementContent data={data} />;
+  } else {
+    return <p>{error}</p>;
+  }
 };
 
 export default AdvertisementContainer;
--- client/src/components/advertisement/AdvertisementContent.tsx
+++ client/src/components/advertisement/AdvertisementContent.tsx
@@ -1,32 +1,37 @@
 // affichage de la donnée
 import { FC } from "react";
 import { Advertisement } from "../../types/advertisement";
-import { Button } from "../ui";
+import { Button } from "../ui/atoms";
 
 type Props = { data: Advertisement };
 
 const AdvertisementContent: FC<Props> = ({ data }) => {
   return (
-      <div className="w-screen h-screen flex flex-column">
-            <div className="box-content w-5/6 m-4 p-4 shadow-md rounded-md">
-                <h1 className="items-center justify-center">{data.name.toUpperCase()}</h1>
-                <span className="text-gray-400 italic">{data.contract_type.replace("_"," ")}</span>
-                <span> - </span>
-                <span className="text-gray-400 italic">{data.place}</span>
-                <hr />
-                <br />
-                <h2>Posted by {data.recruiter.firstname} {data.recruiter.lastname}</h2>
-                <br />
-                <h2>Company {data.company.name}</h2>
-                <br />
-                <h2>Description du Poste</h2>
-                <br />
-                <p>{data.description}</p>
-                <br />
-                <Button text="Apply"/>
-
-            </div>
+    <div className="w-screen h-screen flex flex-column">
+      <div className="box-content w-5/6 m-4 p-4 shadow-md rounded-md">
+        <h1 className="items-center justify-center">
+          {data.name.toUpperCase()}
+        </h1>
+        <span className="text-gray-400 italic">
+          {data.contract_type.replace("_", " ")}
+        </span>
+        <span> - </span>
+        <span className="text-gray-400 italic">{data.place}</span>
+        <hr />
+        <br />
+        <h2>
+          Posted by {data.recruiter.firstname} {data.recruiter.lastname}
+        </h2>
+        <br />
+        <h2>Company {data.company.name}</h2>
+        <br />
+        <h2>Description du Poste</h2>
+        <br />
+        <p>{data.description}</p>
+        <br />
+        <Button text="Apply" />
       </div>
+    </div>
   );
 };
 
